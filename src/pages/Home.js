e3c1af7101f76d80bf3d4bb38b86b80b04bf88a2
sriram <sriramachandran28@gmail.com>
2025-02-18 15:41:45 -0400

    ✨ style(#7): add underline animation to subtitle

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../home.css';
 
-function Home() {
+function  Home() {
   return (
     <section className="home">
       <h1 className="title">Hello I'm Sriram!</h1>
-      <p className="subtitle">I'm a Full-Stack Developer passionate about building impactful applications.</p>
+      <p className="subtitle ">I'm a <span className='underline-text'>Full-Stack Developer</span> passionate about building impactful applications.</p>
     </section>
   );
 }
