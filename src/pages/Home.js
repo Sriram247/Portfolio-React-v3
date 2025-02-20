75ed48b1347129cba7d013d24a0494231697693c
sriram <sriramachandran28@gmail.com>
2025-02-20 00:11:16 -0400

    ✨ feat(#7): add Timeline component and switch component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import '../home.css';
+import Switch from '../components/Switch';
+import WorkTimeline from '../components/WorkTimeline';
+import "../assets/css/home.css";
+
+
 
 function  Home() {
   return (
+    <>
     <section className="home">
       <h1 className="title">Hello I'm Sriram!</h1>
-      <p className="subtitle ">I'm a <span className='underline-text'>Full-Stack Developer</span> passionate about building impactful applications.</p>
+      <Switch/>
+      <WorkTimeline />  
+
+   
+
     </section>
+    </>
   );
 }
 
